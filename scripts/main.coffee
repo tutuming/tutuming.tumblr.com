804eb6eeabc610f18920b255ca4388d2f986794a
tutuming <tutuming@gmail.com>
2012-03-04 02:27:27 +0900

    delete

--- scripts/main.coffee
+++
@@ -1,6 +0,0 @@
-$ ->
-  SyntaxHighlighter.config.bloggerMode = false
-  SyntaxHighlighter.config.stripBrs = true
-  SyntaxHighlighter.defaults['wrap-lines'] = false
-  SyntaxHighlighter.config.clipboardSwf = 'https://raw.github.com/tutuming/tutuming.tumblr.com/master/scripts/sh/clipboard.swf'
-  SyntaxHighlighter.all()
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,2 @@
 
-$(function() {
-  SyntaxHighlighter.config.bloggerMode = false;
-  SyntaxHighlighter.config.stripBrs = true;
-  SyntaxHighlighter.defaults['wrap-lines'] = false;
-  SyntaxHighlighter.config.clipboardSwf = 'https://raw.github.com/tutuming/tutuming.tumblr.com/master/scripts/sh/clipboard.swf';
-  return SyntaxHighlighter.all();
-});
+
