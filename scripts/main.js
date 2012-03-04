29bbbeed68c6ea2e590f36564c42c8ae453e4af3
tutuming <tutuming@gmail.com>
2012-03-04 11:50:48 +0900

    change

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,9 @@ onload = function() {};
 
 yepnope([
   {
-    load: ["http://tutuming.github.com/tutuming.tumblr.com/styles/main.css", "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shCore.css", "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shThemeRDark.css", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shCore.js"]
+    load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/XRegExp.js", "http://tutuming.github.com/tutuming.tumblr.com/styles/main.css", "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shCore.css", "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shThemeRDark.css"]
   }, {
+    load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shCore.js"],
     load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushJScript.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushXml.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushPython.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushCoffeeScript.js"]
   }, {
     complete: function() {
