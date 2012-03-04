b6cd688fb0dff84cd97f54a7e156447f6ea39bb0
tutuming <tutuming@gmail.com>
2012-03-04 15:18:50 +0900

    require使うように

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,16 @@
-
-yepnope({
-  load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/XRegExp.js", "http://tutuming.github.com/tutuming.tumblr.com/styles/main.css", "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shCore.css", "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shThemeFadeToGrey.css"],
-  complete: function() {
-    return yepnope({
-      load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shCore.js"],
-      complete: function() {
-        return yepnope({
-          load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushJScript.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushXml.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushPython.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushCoffeeScript.js"],
-          complete: function() {
-            SyntaxHighlighter.config.bloggerMode = false;
-            SyntaxHighlighter.config.stripBrs = true;
-            SyntaxHighlighter.defaults['wrap-lines'] = false;
-            return SyntaxHighlighter.all();
-          }
-        });
-      }
+require([
+  "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/XRegExp.js",
+  "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
+],function(){
+  require(["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shCore.js"], function(){
+    require(["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushJScript.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushXml.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushPython.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushCoffeeScript.js"],function(){
+      $(function(){
+        SyntaxHighlighter.config.bloggerMode = false;
+        SyntaxHighlighter.config.stripBrs = true;
+        SyntaxHighlighter.defaults['wrap-lines'] = false;
+        SyntaxHighlighter.highlight();
+      });
     });
-  }
+  });
 });
+
