fbe8c26af444c67fd58b595cb91e09712fc7884c
tutuming <tutuming@gmail.com>
2012-03-04 12:05:44 +0900

    change

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -2,21 +2,22 @@ onload = ->
   SyntaxHighlighter.config.bloggerMode = false
   SyntaxHighlighter.config.stripBrs = true
   SyntaxHighlighter.defaults['wrap-lines'] = false
-  SyntaxHighlighter.all()
+  SyntaxHighlighter.highlight()
 
-yepnope [{
+yepnope
   load : ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/XRegExp.js"
           "http://tutuming.github.com/tutuming.tumblr.com/styles/main.css"
           "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
           "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shCore.css"
           "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shThemeRDark.css" ]
-},{
-  load : [ "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shCore.js" ]
-},{
-  load :["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushJScript.js"
-         "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushXml.js"
-         "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushPython.js"
-         "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushCoffeeScript.js"]
   complete : ->
-    $(onload)
-}]
+    yepnope
+      load : [ "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shCore.js" ]
+      complete : ->
+        yepnope
+          load : ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushJScript.js"
+                  "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushXml.js"
+                  "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushPython.js"
+                  "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushCoffeeScript.js"]
+          complete : ->
+            $(onload)
--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,22 @@ onload = function() {
   SyntaxHighlighter.config.bloggerMode = false;
   SyntaxHighlighter.config.stripBrs = true;
   SyntaxHighlighter.defaults['wrap-lines'] = false;
-  return SyntaxHighlighter.all();
+  return SyntaxHighlighter.highlight();
 };
 
-yepnope([
-  {
-    load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/XRegExp.js", "http://tutuming.github.com/tutuming.tumblr.com/styles/main.css", "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shCore.css", "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shThemeRDark.css"]
-  }, {
-    load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shCore.js"]
-  }, {
-    load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushJScript.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushXml.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushPython.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushCoffeeScript.js"],
-    complete: function() {
-      return $(onload);
-    }
+yepnope({
+  load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/XRegExp.js", "http://tutuming.github.com/tutuming.tumblr.com/styles/main.css", "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js", "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shCore.css", "http://tutuming.github.com/tutuming.tumblr.com/styles/sh/shThemeRDark.css"],
+  complete: function() {
+    return yepnope({
+      load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shCore.js"],
+      complete: function() {
+        return yepnope({
+          load: ["http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushJScript.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushXml.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushPython.js", "http://tutuming.github.com/tutuming.tumblr.com/scripts/sh/shBrushCoffeeScript.js"],
+          complete: function() {
+            return $(onload);
+          }
+        });
+      }
+    });
   }
-]);
+});
